ce2be90c1d87997be9950e282649ee6871447dd1
uroboro <uroboro845@gmail.com>
2015-03-29 15:03:39 -0300

    Remove unused interface information

--- interfaces.h
+++ interfaces.h
@@ -3,117 +3,46 @@
 
 #import <UIKit/UIKit.h>
 
+@interface SBIconListView : UIView
+- (id)icons;
+@end
+
 @interface SBIconController : NSObject
-+(id)sharedInstance;
--(id)model;
--(id)openFolder;
--(char)hasOpenFolder;
--(char)hasAnimatingFolder;
--(void)closeFolderAnimated:(char)arg1;
--(void)openFolder:(id)arg1 animated:(char)arg2;
--(id)currentRootIconList;
++ (id)sharedInstance;
+- (id)model;
+- (id)openFolder;
+- (BOOL)hasOpenFolder;
+- (BOOL)hasAnimatingFolder;
+- (void)closeFolderAnimated:(BOOL)animated;
+- (void)openFolder:(id)arg1 animated:(BOOL)animated;
+- (SBIconListView *)currentRootIconList;
 @end
 
 @interface SBIconModel : NSObject
--(id)leafIcons;
-@end
-
-@interface SBIconListView : UIView
--(id)icons;
+- (void)loadAllIcons;
 @end
 
 @interface SBIconViewMap : NSObject
-+(id)homescreenMap;
--(id)mappedIconViewForIcon:(id)arg1;
-@end
-
-@interface SBApplication : NSObject
-@end
-
-@interface SBApplicationController : NSObject
-+(id)sharedInstance;
--(id)applicationWithDisplayIdentifier:(id)arg1;
--(id)applicationWithBundleIdentifier:(id)arg1;
++ (id)homescreenMap;
+- (id)mappedIconViewForIcon:(id)arg1;
 @end
 
 @interface SBIcon : NSObject
--(NSInteger)badgeValue;
--(id)badgeNumberOrString;
--(void)setBadge:(id)arg1;
--(id)application;
--(NSString *)displayName;
--(id)getIconImage:(int)arg1;
--(void)reloadIconImagePurgingImageCache:(char)arg1;
--(char)isApplicationIcon;
--(char)isBookmarkIcon;
--(char)isLeafIcon;
--(char)isDownloadingIcon;
--(char)isPrintStatusIcon;
--(char)isNewsstandApplicationIcon;
--(char)isNewsstandIcon;
--(char)isFolderIcon;
-@end
-
-@interface SBPlaceholderIcon : SBIcon
-@end
-
-@interface SBFolderIcon : SBIcon
-@end
-
-@interface SBNewsstandIcon : SBFolderIcon
-@end
-
-@interface SBLeafIcon : SBIcon
-@end
-
-@interface SBBookmarkIcon : SBLeafIcon
-@end
-
-@interface SBDownloadingIcon : SBLeafIcon
-@end
-
-@interface SBPrintStatusIcon : SBLeafIcon
-@end
-
-@interface SBApplicationIcon : SBLeafIcon
--(id)initWithApplication:(id)arg1;
-@end
-
-@interface SBCalendarApplicationIcon : SBApplicationIcon
-@end
-
-@interface SBWeatherApplicationIcon : SBApplicationIcon
-@end
-
-@interface SBWebApplicationIcon : SBApplicationIcon
-@end
-
-@interface SBUserInstalledApplicationIcon : SBApplicationIcon
-@end
-
-@interface SBNewsstandApplicationIcon : SBUserInstalledApplicationIcon
-@end
-
-@interface SBIconImageView : UIImageView
+- (NSInteger)badgeValue;
+- (id)badgeNumberOrString;
+- (void)setBadge:(id)arg1;
+- (NSString *)displayName;
+- (id)getIconImage:(NSInteger)arg1;
+- (void)reloadIconImagePurgingImageCache:(BOOL)arg1;
 @end
 
 @interface SBIconView : UIView
 @property (nonatomic, assign) SBIcon *icon;
-+(CGSize)defaultIconSize;
-+(CGSize)defaultIconImageSize;
--(id)initWithDefaultSize;
--(SBIconImageView *)iconImageView;
--(void)updateLabel; // < iOS 7
--(void)_updateLabel; // >= iOS 7
-@end
-
-@interface SBDownloadingIconView : SBIconView
-@end
-
-@interface SBFolderIconView : SBIconView
-@end
-
-@interface SBNewsstandIconView : SBFolderIconView
++ (CGSize)defaultIconSize;
++ (CGSize)defaultIconImageSize;
+- (id)initWithDefaultSize;
+- (void)updateLabel; // < iOS 7
+- (void)_updateLabel; // >= iOS 7
 @end
 
 @interface BBBulletinRequest : NSObject {}
@@ -130,19 +59,19 @@
 
 @interface SBBulletinBannerController : NSObject
 + (id)sharedInstance;
-- (void)_presentBannerForItem:(id)arg1; // <= kCFCoreFoundationVersionNumber_iOS_5_1
-- (id)newBannerViewForItem:(id)arg1; // > kCFCoreFoundationVersionNumber_iOS_5_1
-- (void)observer:(id)arg1 addBulletin:(BBBulletinRequest *)arg2 forFeed:(NSUInteger)arg3;
+- (void)_presentBannerForItem:(id)arg1; // iOS 5
+- (id)newBannerViewForItem:(id)arg1; // iOS 6
+- (void)observer:(id)arg1 addBulletin:(BBBulletinRequest *)arg2 forFeed:(NSUInteger)arg3; // iOS 7-8
 @end
 
 @interface SBBulletinBannerItem : NSObject
-+ (id)itemWithBulletin:(id)arg1; // <= kCFCoreFoundationVersionNumber_iOS_5_1
-+ (id)itemWithBulletin:(id)arg1 andObserver:(id)arg2; // > kCFCoreFoundationVersionNumber_iOS_5_1
++ (id)itemWithBulletin:(id)arg1; // iOS 5
++ (id)itemWithBulletin:(id)arg1 andObserver:(id)arg2; // iOS 6
 @end
 
 @interface SBBannerController : NSObject
 + (id)sharedInstance;
-- (void)_presentBannerView:(id)arg1; // > kCFCoreFoundationVersionNumber_iOS_5_1
+- (void)_presentBannerView:(id)arg1; // iOS 7-8
 @end
 
 #endif /* INTERFACES_H */
